c9b22fbeae8dc4f1dd268bdd53ac08c2e757a039
Edgina Julievani <trifoliumginaa@gmail.com>
2023-10-22 22:24:52 +0700

    halamankedia

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -10,23 +10,54 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 233, 233, 233),
-      body: SingleChildScrollView(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image(
-                  height: 200,
-                  width: 200,
-                  image: AssetImage("assets/images/buku.png")),
-   ]),
+    return Scaffold(
+      backgroundColor: Color.fromARGB(108, 202, 139, 139),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Container(
+              padding: const EdgeInsets.only(right: 20, left: 15, top: 15),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(right: 25, left: 15, top: 30),
+              child: Column(
+                children: [
+                  Text(
+                    "Our Product",
+                    style: TextStyle(
+                      fontFamily: "kaisei",
+                      fontSize: 30,
+                      color: const Color.fromARGB(255, 12, 12, 12),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ]),
         ),
-      )),
+      ),
     );
   }
 }
